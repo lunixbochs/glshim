e87358be1182728a7d68d317e1a33a3fa6e11504
Ryan Hileman <lunixbochs@gmail.com>
2014-07-16 19:22:27 -0700

    fix some tests

--- test/tests/block/quads.c
+++ test/tests/block/quads.c
@@ -1,19 +1,49 @@
 int main() {
     glBegin(GL_QUADS);
-    glVertex3f(0, 1, 2);
     glColor4f(0, 0.1, 0.2, 0.3);
-    glTexCoord2f(.4, .5);
+    glTexCoord2f(0.4, 0.5);
+    glVertex3f(0, 1, 2);
+    glVertex3f(3, 4, 5);
+    glVertex3f(6, 7, 8);
+    glVertex3f(9, 10, 11);
     glEnd();
+
+    GLfloat verts[] = {
+        0, 1, 2,
+        3, 4, 5,
+        6, 7, 8,
+        9, 10, 11,
+    };
+
+    GLfloat color[] = {
+        0, 0.1, 0.2, 0.3,
+        0, 0.1, 0.2, 0.3,
+        0, 0.1, 0.2, 0.3,
+        0, 0.1, 0.2, 0.3,
+    };
+
+    GLfloat tex[] = {
+        0.4, 0.5,
+        0.4, 0.5,
+        0.4, 0.5,
+        0.4, 0.5,
+    };
+
+    GLushort indices[] = {
+        0, 1, 3,
+        1, 2, 3,
+    };
 
     // TODO: out of order glEnableClientState?
     test_glColor4f(0.0, 0.1, 0.2, 0.3);
+
     test_glEnableClientState(GL_VERTEX_ARRAY);
-    test_glVertexPointer(3, GL_FLOAT, 0, NULL);
+    test_glVertexPointer(3, GL_FLOAT, 0, verts);
     test_glEnableClientState(GL_COLOR_ARRAY);
-    test_glColorPointer(4, GL_FLOAT, 0, NULL);
+    test_glColorPointer(4, GL_FLOAT, 0, color);
     test_glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-    test_glTexCoordPointer(2, GL_FLOAT, 0, NULL);
-    test_glDrawElements(4, 1, GL_UNSIGNED_SHORT, NULL);
+    test_glTexCoordPointer(2, GL_FLOAT, 0, tex);
+    test_glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, indices);
 
     test_glDisableClientState(GL_VERTEX_ARRAY);
     test_glDisableClientState(GL_COLOR_ARRAY);
--- test/tests/block/rect.c
+++ test/tests/block/rect.c
@@ -5,10 +5,13 @@ int main() {
         test_glEnableClientState(GL_VERTEX_ARRAY);
         // TODO: pointers are skipped in verification
         GLfloat verts[] = {
-            0, 0, 0, 0, 0, 0,
+            0, 0, 0,
+            1, 0, 0,
+            1, 1, 0,
+            0, 1, 0,
         };
         test_glVertexPointer(3, GL_FLOAT, 0, verts);
-        test_glDrawArrays(6, 0, 4);
+        test_glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
         test_glDisableClientState(GL_VERTEX_ARRAY);
     }
     mock_return;
