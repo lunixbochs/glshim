e259f7bfb1c140b504a707d82c9f9746b9e67115
Ryan Hileman <lunixbochs@gmail.com>
2014-07-18 01:50:50 -0700

    move glGet* to get.c

---
+++ src/gl/get.c
@@ -0,0 +1,46 @@
+#include "gl.h"
+
+// config functions
+const GLubyte *glGetString(GLenum name) {
+    LOAD_GLES(glGetString);
+    switch (name) {
+        case GL_VERSION:
+#ifdef USE_ES2
+            return (GLubyte *)"4.3 glshim wrapper";
+#else
+            return (GLubyte *)"1.4 glshim wrapper";
+#endif
+        case GL_EXTENSIONS:
+            return (const GLubyte *)(char *){
+#ifndef USE_ES2
+                // "GL_ARB_vertex_buffer_object "
+                "GL_ARB_multitexture "
+                "GL_ARB_texture_cube_map "
+                "GL_EXT_secondary_color "
+                "GL_EXT_texture_env_combine "
+                "GL_EXT_texture_env_dot3 "
+#else
+                "GL_ARB_vertex_shader "
+                "GL_ARB_fragment_shader "
+                "GL_ARB_vertex_buffer_object "
+                "GL_EXT_framebuffer_object "
+#endif
+            };
+        default:
+            return gles_glGetString(name);
+    }
+}
+
+void glGetIntegerv(GLenum pname, GLint *params) {
+    LOAD_GLES(glGetIntegerv);
+    switch (pname) {
+        case GL_MAX_ELEMENTS_INDICES:
+            *params = 1024;
+            break;
+        case GL_AUX_BUFFERS:
+            *params = 0;
+            break;
+        default:
+            gles_glGetIntegerv(pname, params);
+    }
+}
