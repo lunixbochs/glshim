f6b80abb4cb3b10d2368bfd51a2727d93c403e1c
Ryan Hileman <lunixbochs@gmail.com>
2014-07-19 06:49:17 -0700

    add gl_str to glLightModel stub

--- src/gl/light.c
+++ src/gl/light.c
@@ -1,3 +1,4 @@
+#include "gl_str.h"
 #include "light.h"
 #include "loader.h"
 
@@ -9,7 +10,7 @@ void glLightModelf(GLenum pname, GLfloat param) {
         case GL_LIGHT_MODEL_TWO_SIDE:
             gles_glLightModelf(pname, param);
         default:
-            printf("stubbed glLightModelf(%i, %.2f)\n", pname, param);
+            printf("stubbed glLightModelf(%s, %.2f)\n", gl_str(pname), param);
             break;
     }
 }
